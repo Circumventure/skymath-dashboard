ba368b2d464155b56ac2b5a95a06a0e489f55baa
missyjcat <missyjcat@gmail.com>
2015-11-12 08:44:11 -0800

    Add conditional logic for API_URL

--- dashboard-app/src/components/App.js
+++ dashboard-app/src/components/App.js
@@ -6,10 +6,11 @@ var DashboardApp = require('DashboardApp');
 
 // Production API URL
 // TODO(jchan): Add conditional logic when we have SkyMath production environment
-// API_URL = 'http://api.skymath.com/api';
-
-// Staging API URL
-API_URL = 'http://104.131.82.183/api'
+if (window.location.hostname === '104.131.82.183') {
+    API_URL = 'http://api.skymath.com/api';
+} else {
+    API_URL = 'http://104.131.82.183/api'
+}
 
 var adminAppInstance = React.render(
     <DashboardApp />,
