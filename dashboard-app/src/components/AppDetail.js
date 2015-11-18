c48967162d4fae1a6ec89048265ad487c99ad3df
missyjcat <missyjcat@gmail.com>
2015-11-18 07:14:42 -0800

    Adding ability to see images in app upload

--- dashboard-app/src/components/AppDetail.js
+++ dashboard-app/src/components/AppDetail.js
@@ -51,6 +51,13 @@ var AppDetail = React.createClass({
         return false;
     },
 
+    isImageUrlField: function(fieldname) {
+        if (fieldname.indexOf('_icon_url') > -1) {
+            return true;
+        }
+        return false;
+    },
+
     componentWillMount: function() {
         var newState = {};
         if (this.props && this.props.data) {
@@ -69,7 +76,20 @@ var AppDetail = React.createClass({
             if (this.isFileField(field)) {
                 return (
                     <InputFile inputClass="{'input ' + field}" labelClass="label" className="fieldset fieldset--addedit" onChange={this.handleChange} label={field} id={field} />
-                )
+                );
+            }
+            if (this.isImageUrlField(field)) {
+                return (
+                    <div>
+                        <InputText className="fieldset fieldset--addedit" labelClass="label" inputClass={field + ' input'} id={field} label={field} value={this.state[field]} onChange={this.handleChange} />
+                        <div className="imageContainer" id={field + '_container'} style={{display: 'inline-block'}}>
+                            {this.state[field] ? [<img src={this.state[field]} id={field + '_image'} />] : null}
+                        </div>
+                        <div className="canvasContainer" id={field + '_canvas_container'} style={{display: 'inline-block'}}>
+                            <canvas id={field + '_canvas'}></canvas>
+                        </div>
+                    </div>
+                );
             }
             return (
                 <InputText className="fieldset fieldset--addedit" labelClass="label" inputClass={field + ' input'} id={field} label={field} value={this.state[field]} onChange={this.handleChange} />
@@ -99,8 +119,10 @@ var AppDetail = React.createClass({
             var re = /data\:image\/\w{2,}\;base64\,/;
             var match;
             var targetId = event.target.id;
-            var mimeFieldName = event.target.id.split('_')[0] + '_image_mime';
+            var appNumber = targetId.split('_')[0];
+            var mimeFieldName = appNumber + '_image_mime';
             var fr = new FileReader();
+            var file = event.target.files[0];
             fr.onloadend = function() {
                 data = fr.result;
                 match = data.match(re);
@@ -109,9 +131,35 @@ var AppDetail = React.createClass({
                 }
                 newValue[targetId] = data;
                 newValue[mimeFieldName] = match[0].split(';')[0].split(':')[1];
+
+                // Clear out current values
+                newValue[appNumber + '_icon_url'] = '';
+
+                // Draw the image onto the canvas element
+                var canvas = document.getElementById(appNumber + '_icon_url_canvas');
+                if (canvas) {
+                    var ctx = canvas.getContext('2d');
+                    ctx.clearRect(0, 0, canvas.width, canvas.height);
+                } else {
+                    // Hacky, not sure why canvas element is disappearing
+                    $('#' + appNumber + '_icon_url_container').html('<canvas id="' + appNumber + '_icon_url_canvas"></canvas>');
+                    canvas = document.getElementById(appNumber + '_icon_url_canvas');
+                    var ctx = canvas.getContext('2d');
+                    ctx.clearRect(0, 0, canvas.width, canvas.height);
+                }
+                var img = new Image;
+                img.src = URL.createObjectURL(file);
+                img.onload = function() {
+                    ctx.drawImage(img, 20,20);
+                }
+
                 this.setState(newValue);
             }.bind(this);
-            fr.readAsDataURL(event.target.files[0]);
+            fr.readAsDataURL(file);
+
+        } else if (this.isImageUrlField(event.target.id)) {
+            newValue[event.target.id] = event.target.value;
+            this.setState(newValue)
         } else {
             newValue[event.target.id] = event.target.value;
             this.setState(newValue);
@@ -119,7 +167,12 @@ var AppDetail = React.createClass({
     },
 
     handleSave: function() {
-        var data = this.state;
+        var data = $.extend({},this.state);
+        for (var i = 1; i < 5; i++) {
+            if (data['app' + i + '_image_upload']) {
+                delete data['app' + i + '_icon_url'];
+            }
+        }
         Utils.Store.makeCall('updateIsland', data);
         this.backToOverview();
     },
